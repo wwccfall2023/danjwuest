5bb24e5a5159f8ae8e3511a4ee4099d0a9a35d58
danjwuest <121892498+danjwuest@users.noreply.github.com>
2023-12-06 18:17:20 -0800

    Initial Table Creation

--- week7/answers.sql
+++ week7/answers.sql
@@ -1,3 +1,106 @@
 -- Create your tables, views, functions and procedures here!
 CREATE SCHEMA destruction;
 USE destruction;
+
+CREATE TABLE players (
+  player_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30)
+  email TEXT
+);
+
+CREATE TABLE characters (
+  character_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  player_id INT UNSIGNED,
+  name VARCHAR(30),
+  -- Level *shouldn't* be negative, right?
+  level INT UNSIGNED,
+  CONSTRAINT characters_fk_players
+    FOREIGN KEY (player_id)
+    REFERENCES players (player_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+);
+
+CREATE TABLE winners (
+  character_id INT UNSIGNED PRIMARY KEY,
+  name VARCHAR(30),
+  CONSTRAINT winners_fk_characters
+    FOREIGN KEY (character_id)
+    REFERENCES characters (character_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+);
+
+CREATE TABLE character_stats (
+  character_id INT UNSIGNED PRIMARY KEY,
+  health INT UNSIGNED,
+  armor INT UNSIGNED,
+  CONSTRAINT char_stars_fk_characters
+    FOREIGN KEY (character_id)
+    REFERENCES characters (character_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+);
+
+CREATE TABLE teams (
+  team_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30)
+);
+
+CREATE TABLE team_members (
+  team_member_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  team_id INT UNSIGNED,
+  character_id INT UNSIGNED,
+  CONSTRAINT team_members_fk_teams
+    FOREIGN KEY (team_id)
+    REFERENCES teams (team_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE,
+  CONSTRAINT team_members_fk_characters
+    FOREIGN KEY (character_id)
+    REFERENCES teams (character_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+);
+
+CREATE TABLE items (
+  item_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30),
+  -- Leaving armor signed because armor could potentially be a negative debuff.
+  armor INT,
+  damage INT UNSIGNED
+);
+
+CREATE TABLE inventory (
+  inventory_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  character_id INT UNSIGNED,
+  item_id INT UNSIGNED,
+  CONSTRAINT inventory_fk_characters
+    FOREIGN KEY (character_id)
+    REFERENCES characters (character_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE,
+  CONSTRAINT inventory_fk_items
+    FOREIGN KEY (item_id)
+    REFERENCES teams (item_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+);
+
+CREATE TABLE equipped (
+  equipped_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  character_id INT UNSIGNED,
+  item_id INT UNSIGNED,
+  CONSTRAINT equipped_fk_characters
+    FOREIGN KEY (character_id)
+    REFERENCES characters (character_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE,
+  CONSTRAINT equipped_fk_items
+    FOREIGN KEY (item_id)
+    REFERENCES teams (item_id)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+);
+
