f431117b4474ebd74c078d51535b78a694dd2446
danjwuest <121892498+danjwuest@users.noreply.github.com>
2023-12-15 14:23:28 -0800

    Adding triggers for updating sessions, posts, and adding new users

--- week9/answers.sql
+++ week9/answers.sql
@@ -24,6 +24,15 @@ CREATE TABLE sessions (
       ON DELETE CASCADE
 );
 
+DELIMITER ;;
+CREATE TRIGGER session_update_time
+	AFTER UPDATE ON sessions
+	FOR EACH ROW
+BEGIN
+	UPDATE sessions SET updated_on = CURRENT_TIMESTAMP WHERE session_id = NEW.session_id;
+END;;
+DELIMITER ;
+
 CREATE TABLE friends (
 	user_friend_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	user_id INT UNSIGNED NOT NULL,
@@ -53,6 +62,15 @@ CREATE TABLE posts (
 			ON DELETE CASCADE
 );
 
+DELIMITER ;;
+CREATE TRIGGER post_update_time
+	AFTER UPDATE ON posts
+	FOR EACH ROW
+BEGIN
+	UPDATE posts SET updated_on = CURRENT_TIMESTAMP WHERE post_id = NEW.post_id;
+END;;
+DELIMITER ;
+
 CREATE TABLE notifications (
 	notification_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	user_id INT UNSIGNED NOT NULL,
@@ -68,3 +86,46 @@ CREATE TABLE notifications (
 			ON UPDATE CASCADE
 			ON DELETE CASCADE
 );
+
+DELIMITER ;;
+CREATE TRIGGER notification_new_user
+	AFTER INSERT ON users
+	FOR EACH ROW
+BEGIN
+	DECLARE join_post_id INT;
+    DECLARE current_notification_user_id INT;
+    DECLARE row_not_found TINYINT DEFAULT FALSE;
+    
+    DECLARE users_cursor CURSOR FOR
+		SELECT user_id
+			FROM users;
+	
+    DECLARE CONTINUE HANDLER FOR NOT FOUND
+		SET row_not_found = TRUE;
+	
+	-- I looked up how to put data into strings here:
+	-- https://stackoverflow.com/questions/17754188/how-to-concatenate-variables-into-sql-strings
+	-- Then instead used normal concatenation instead:
+	-- https://www.w3schools.com/sql/func_sqlserver_concat.asp
+	INSERT INTO posts (user_id, content) VALUES (NEW.user_id, CONCAT(NEW.first_name,' ',NEW.last_name, 'just joined!'));
+    
+    -- Found a way to get the ID of the last insert's auto increment:
+    -- https://docs.oracle.com/cd/E17952_01/connector-odbc-en/connector-odbc-usagenotes-functionality-last-insert-id.html#:~:text=To%20obtain%20the%20value%20immediately,obtain%20the%20auto%2Dincrement%20value.
+    SET join_post_id = LAST_INSERT_ID();
+    
+    OPEN users_cursor;
+    user_loop : LOOP
+		FETCH users_cursor INTO current_notification_user_id;
+        
+        IF row_not_found THEN
+        
+			LEAVE user_loop;
+		END IF;
+        
+        IF current_notification_user_id != NEW.user_id THEN
+			-- Makes sure the user doesn't get a notification of their own joining.
+			INSERT INTO notifications (user_id, post_id) VALUES (current_notification_user_id, join_post_id);
+		END IF;
+	END LOOP user_loop;
+END;;
+DELIMITER ;
